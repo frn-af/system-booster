cfaf8571516636bf4ea855c11175e121aac192bd
frn-af <1911512017_farhan@student.unand.ac.id>
2023-05-21 15:34:40 +0700

    Connect monitoring with Firebase

--- components/monitoring/Monitoring.tsx
+++ components/monitoring/Monitoring.tsx
@@ -1,33 +1,68 @@
 import { Switch, useColorScheme } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./monitoring.style";
 import { Text, View } from "../Themed";
 import Colors from "../../constants/Colors";
-
-const currentDate = new Date();
-const date = currentDate.getDate();
-const month = currentDate.toLocaleString("en-US", { month: "long" });
-const year = currentDate.getFullYear();
+import { collection, doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { FIREBASE_DB } from "../../config/FirebaseConfig";
 
 export default function Monitoring() {
   const [isEnabled, setIsEnabled] = useState(false);
   const [text, setText] = useState("Offline");
-
+  const [fermentasi, setFermentasi] = useState();
+  const [time, setTime] = useState();
+  const [status, setStatus] = useState("Sistem Offline");
   const colorScheme = useColorScheme();
+
+  //make a function for update data
+  const updateData = async () => {
+    const timeRef = collection(FIREBASE_DB, "tools");
+    const q = doc(timeRef, "monitoring");
+    const data = {
+      kontrol: !isEnabled,
+    };
+    await updateDoc(q, data);
+  };
+
+  useEffect(() => {
+    const timeRef = collection(FIREBASE_DB, "tools");
+
+    const q = doc(timeRef, "time");
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const data: any = snapshot.data();
+      setFermentasi(data.fermentasi);
+      setTime(data.timestamp);
+    });
+
+    const q2 = doc(timeRef, "monitoring");
+    const unsubscribe2 = onSnapshot(q2, (snapshot) => {
+      const data: any = snapshot.data();
+      setIsEnabled(data.kontrol);
+      setText(data.kontrol ? "Online" : "Offline");
+      if (data.kontrol == false) {
+        setStatus("Sistem Offline");
+      } else if (data.kontrol == true && data.ph <= 4) {
+        setStatus("Tapai Sudah Matang");
+      } else {
+        setStatus("Tapai Belum Matang");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+      unsubscribe2();
+    };
+  }, []);
 
   const toggleSwitch = () => {
     setIsEnabled((previousState) => !previousState);
-    if (isEnabled) {
-      setText("Offline");
-    } else {
-      setText("Online");
-    }
+    updateData();
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.status}>status</Text>
-      <Text style={styles.tapestatus}>Tapai Belum Matang</Text>
+      <Text style={styles.tapestatus}>{status}</Text>
       <View style={styles.sisteminfo}>
         <Text style={styles.textlabel}>Aktivasi {`\n`}Sistem</Text>
         <Text style={styles.textlabel}>
@@ -46,15 +81,14 @@ export default function Monitoring() {
           }}
         />
       </View>
+
       <View style={styles.timelabel}>
         <Text style={styles.textlabel}>tanggal</Text>
-        <Text style={styles.timeinfo}>
-          {month} {date}, {year}
-        </Text>
+        <Text style={styles.timeinfo}>{time}</Text>
       </View>
       <View style={styles.timelabel}>
         <Text style={styles.textlabel}>lama {`\n`}fermantasi</Text>
-        <Text style={styles.timeinfo}>20 jam 5 menit</Text>
+        <Text style={styles.timeinfo}>{fermentasi}</Text>
       </View>
     </View>
   );
