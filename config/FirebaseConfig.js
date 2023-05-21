96a7b1582b86df9e05e678cacd9fb297ed164bb2
frn-af <1911512017_farhan@student.unand.ac.id>
2023-05-21 23:07:29 +0700

    Secure Secret

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -6,15 +6,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAQqn8sTOO34RnbW5AWfegClYKqe-XM8E4",
-  authDomain: "tugas-akhir-997ec.firebaseapp.com",
-  projectId: "tugas-akhir-997ec",
-  storageBucket: "tugas-akhir-997ec.appspot.com",
-  messagingSenderId: "1055206685852",
-  appId: "1:1055206685852:web:60cff78c02a917392b8159",
-  measurementId: "G-6L1X7PPWHP",
-};
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
