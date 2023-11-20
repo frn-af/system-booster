026ba616622c7a2a8fb87b6e8f244e1581f90262
Farhan Alfathra <92733733+frn-af@users.noreply.github.com>
2023-11-20 19:19:00 +0700

    Update FirebaseConfig.js

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAQqn8sTOO34RnbW5AWfegClYKqe-XM8E4",
-  authDomain: "tugas-akhir-997ec.firebaseapp.com",
-  projectId: "tugas-akhir-997ec",
-  storageBucket: "tugas-akhir-997ec.appspot.com",
-  messagingSenderId: "1055206685852",
-  appId: "1:1055206685852:web:60cff78c02a917392b8159",
-  measurementId: "G-6L1X7PPWHP",
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  measurementId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
 
 // Initialize Firebase
